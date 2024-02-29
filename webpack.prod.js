203c8e0c9027744a6518fee6893a221fb0bdedb4
IbadAsadov <68506261+IbadAsadov@users.noreply.github.com>
2024-02-28 22:34:49 -0800

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -25,7 +25,7 @@ module.exports = {
                 use: [MiniCssExtractPlugin.loader, "css-loader"],
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
+                test: /\.(png|jpe?g|gif|svg)$/i,
                 loader: "file-loader",
                 options: {
                     name: "[name].[ext]",
